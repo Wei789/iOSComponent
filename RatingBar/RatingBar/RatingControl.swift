2a5ab35c5aa0b32881a97d659a09e8c80624ab37
鄭惟臣 <weichen.cheng@thinkpower-info.com>
2017-10-03 02:01:59 +0800

    Rating bar

---
+++ RatingBar/RatingBar/RatingControl.swift
@@ -0,0 +1,121 @@
+//
+//  RatingControl.swift
+//  RatingBar
+//
+//  Created by 鄭惟臣 on 2017/10/1.
+//  Copyright © 2017年 CowCow. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable  class RatingControl: UIStackView {
+    //Mark: Properties
+    private var ratingButtons = [UIButton]()
+    
+    @IBInspectable var rating: Double = 0.0 {
+        didSet {
+            setupButtons()
+        }
+    }
+    @IBInspectable var fillImage: UIImage?
+        {
+        didSet {
+            setupButtons()
+        }
+    }
+    @IBInspectable var halfImage: UIImage?
+        {
+        didSet {
+            setupButtons()
+        }
+    }
+    @IBInspectable var emptyImage: UIImage? {
+        didSet {
+            setupButtons()
+        }
+    }
+    @IBInspectable var starSize: CGSize = CGSize(width: 44.0, height: 44.0) {
+        didSet {
+            setupButtons()
+        }
+    }
+    
+    @IBInspectable var starCount: Int = 5 {
+        didSet {
+            setupButtons()
+        }
+    }
+    
+    //Mark: Initalization
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupButtons()
+    }
+    
+    required init(coder: NSCoder) {
+        super.init(coder: coder)
+        setupButtons()
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesMoved(touches, with: event)
+        if let touch = touches.first {
+            updateButtonSelectionStates(point: touch.location(in: self))
+        }
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesMoved(touches, with: event)
+        if let touch = touches.first {
+            updateButtonSelectionStates(point: touch.location(in: self))
+        }
+    }
+    
+    //Mark: Private Methods
+    private func setupButtons() {
+        // clear any existing buttons
+        for button in ratingButtons {
+            removeArrangedSubview(button)
+            button.removeFromSuperview()
+        }
+        
+        ratingButtons.removeAll()
+        
+        for i in 0..<starCount {
+            let button = UIButton()
+            button.heightAnchor.constraint(equalToConstant: starSize.height).isActive = true
+            button.widthAnchor.constraint(equalToConstant: starSize.width).isActive = true
+
+            let ratingStar = Double(i + 1)
+            if rating == ratingStar || rating > ratingStar {
+                button.setImage(fillImage, for: .normal)
+            } else if rating >= ratingStar - 0.5  && rating < ratingStar  {
+                button.setImage(halfImage, for: .normal)
+            } else {
+                button.setImage(emptyImage, for: .normal)
+            }
+            
+            button.adjustsImageWhenHighlighted = false
+            // important, no this line can't detect touchmove event on button
+            button.isUserInteractionEnabled = false
+            addArrangedSubview(button)
+            ratingButtons.append(button)
+        }
+    }
+    
+    private func updateButtonSelectionStates(point: CGPoint) {
+        let halfSize = starSize.width / 2
+        for (index, button) in ratingButtons.enumerated() {
+            if button.frame.minX <= point.x && point.x <= (button.frame.maxX - halfSize) {
+                rating = Double(index + 1) - 0.5
+                button.setImage(halfImage, for: .normal)
+            }else if point.x >= (button.frame.maxX - halfSize) {
+                rating = Double(index + 1)
+                button.setImage(fillImage, for: .normal)
+            }else {
+                button.setImage(emptyImage, for: .normal)
+            }
+        }
+    }
+    
+}
---
+++ RatingBar/RatingBar/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  RatingBar
+//
+//  Created by 鄭惟臣 on 2017/10/1.
+//  Copyright © 2017年 CowCow. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
+
